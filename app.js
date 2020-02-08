8a548f9a33086f6b98f0ca98625dc4e8ca338bc3
John Nichol <johnny@Johns-MacBook-Pro.local>
2020-02-08 15:39:45 -0500

    worked out gender buttons

--- app.js
+++ app.js
@@ -16,23 +16,39 @@ $(() => {
 
     /// Generating class buttons:
     for (var i = 0; i < 9; i++) {
-      const $classButton = $(`<button type="submit" id=\"${data.results[i].index}\" class="button">${data.results[i].index}</button>`)
+      const $classButton = $(`<button type="submit" id=\"${data.results[i].index}\" class="classButton">${data.results[i].index}</button>`)
       $('form').append($classButton)
     }
 
-    /// Attributing click (character class) to $userInput:
-    $('.button').on('click', (event) => {
+    /// Attributing character class choice to $userInput, and calling up class images:
+    $('.classButton').on('click', (event) => {
       event.preventDefault();
       const $userInput = $(event.currentTarget).attr('id');
+      $("#maleImg").attr('src', `imgs/male/${$userInput}.png`)
+      $("#femaleImg").attr('src', `imgs/female/${$userInput}.png`)
       console.log($userInput);
     });
 
+    /// Attributing gender choice to $genderInput:
+    $('.genderButton').on('click', (event) => {
+      event.preventDefault();
+      const $genderInput = $(event.currentTarget).attr('id');
+      console.log($genderInput);
+
+      /// focusing image on gender choice:
+      if ($genderInput === "male") {
+        $("#femaleImg").css('opacity', '0.2')
+        // $("#maleImg").css('transform', 'translate(50px)').css('background-color', 'white')
+        $("#maleImg").css('opacity', '1.2')
+      } else if ($genderInput === "female") {
+          $("#maleImg").css('opacity', '0.2')
+          $("#femaleImg").css('opacity', '1.2')
+      }
+    });
   });
 
 
 
-  // <button type="submit" id="BRONX"      class="button">Bronx     </button>
-  // <button type="submit" id="dragonborn" class="button">dragonborn</button>
-  // const $classButton = $(`<button type="submit" id=\"${data.results[i].index}\" class="button">${data.results[i].index}</button>`)
+
 
 })
