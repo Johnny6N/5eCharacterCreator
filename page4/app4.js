5746e4d6d94f8a1cb97c87709d6fecc118c98e32
John Nichol <jnichol99@gmail.com>
2020-02-10 23:31:39 -0500

    have rolls and race ability modifiers, working on character sheet stats

--- page4/app4.js
+++ page4/app4.js
@@ -1,28 +1,30 @@
+
+
+// Variable carryovers:
 const characterRace = sessionStorage.getItem("$userInput");
 const characterGender = sessionStorage.getItem("$genderInput");
 const characterClass = sessionStorage.getItem("$classInput");
 const characterName = sessionStorage.getItem("$nameInput");
 //
+
+// initializers for roll variables:
 let abilityScore = []
 let sum = []
 rollArray = []
-// $(() => {
-// $('#class').text(characterClass)
-// $('#race').text(characterRace)
-// $('#gender').text(characterGender)
-// $('#name').text(characterName)
-// })
+//
 
+// random roll function for 6-sided die:
 let dieRoll = () => {
   for (var i = 0; i < 4; i++) {
     roll = Math.floor((Math.random() * 6) + 1)
     abilityScore.push(roll);
   }
 }
+//
 
+// Getting all 6 ability score rolls:
 const rollTotal = () => {
-
-for (var i = 0; i < 6; i++) {
+  for (var i = 0; i < 6; i++) {
 
   dieRoll();
   abilityScore.sort().shift();
@@ -32,25 +34,63 @@ for (var i = 0; i < 6; i++) {
 }
 return rollArray;
 }
+//
 
+// loading jQuery and carrying over name/race/gender/class:
 $(() => {
   $('#class').text(characterClass)
   $('#race').text(characterRace)
   $('#gender').text(characterGender)
   $('#name').text(characterName)
+//
 
+// setting roll function to roll button and calling results:
 $('#roll').on('click', (event) => {
   event.preventDefault();
   rollTotal();
-  console.log(rollArray);
   for (var i = 0; i < 6; i++) {
-// console.log(rollArray[i]);
-
     $(`#rollResults${i}`).text(`Roll ${(i+1)}: ${rollArray[i]}`)
-    console.log(`#rollResult${i}`);
   }
-
 })
+//
 
+// getting API for race bonuses to ability scores:
+$.ajax ({
+  url:`http://www.dnd5eapi.co/api/races/${characterRace}`,
 
+}).then ((data) => {
+  for (var i = 0; i < data.ability_bonuses.length; i++) {
+    $(`#abilityBonus${(i+1)}`).text(`${characterRace} gets ${data.ability_bonuses[i].name} + ${data.ability_bonuses[i].bonus}`)
+  }
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    const $strengthInput = $('#strength-input-box').val()
+    $('.strength').append(`<h2>${$strengthInput}`)
+  })
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    const $dexterityInput = $('#dexterity-input-box').val()
+    $('.dexterity').append(`<h2>${$dexterityInput}`)
+  })
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    const $constitutionInput = $('#constitution-input-box').val()
+    $('.constitution').append(`<h2>${$constitutionInput}`)
+  })
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    const $intelligenceInput = $('#intelligence-input-box').val()
+    $('.intelligence').append(`<h2>${$intelligenceInput}`)
+  })
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    const $wisdomInput = $('#wisdom-input-box').val()
+    $('.wisdom').append(`<h2>${$wisdomInput}`)
+  })
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    const $charismaInput = $('#charisma-input-box').val()
+    $('.charisma').append(`<h2>${$charismaInput}`)
+  })
+});
 })
